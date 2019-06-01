d9500364379c95f6dbb4af965afff3c1e7a7b2b2
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-06-01 16:06:31 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.1'
+version = '1.0.2'
 long_description = '\n'.join([
         open("README.rst").read(),
         open("AUTHORS.rst").read(),
